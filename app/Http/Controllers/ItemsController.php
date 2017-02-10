fb3eeb6ce1c0688c2f1d3da39c9c3d96a38d7cf1
Evan Hsu <evanhsu@gmail.com>
2017-02-10 01:02:54 -0700

    Updated vagrant machine with Yarn. Stubbed out ItemsController

---
+++ app/Http/Controllers/ItemsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ItemsController extends Controller
+{
+    //
+    public function indexForCrew($crewId) {
+
+    }
+
+    public function indexForPerson($personId) {
+
+    }
+
+    public function show($id) {
+
+    }
+
+    public function update($id, $request) {
+
+    }
+}
