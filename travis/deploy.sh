89ab53f5290d1ff6eb8cfd8f4f9c0dd5b236bcdd
Evan Hsu <evanhsu@gmail.com>
2017-07-31 03:08:03 -0600

    Added migration back to the deployment script.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -8,3 +8,6 @@ rsync -a --quiet --delete-after --exclude .git* ${TRAVIS_BUILD_DIR}/ ${ssh_user}
 
 ssh ${ssh_user}@${deploy_host} "sudo cp -a ~/deploy/. ${deploy_path}/"
 ssh ${ssh_user}@${deploy_host} "sudo chown -RP --quiet --preserve-root ${site_owner}:${site_owner} ${deploy_path}"
+
+# Migrate the database
+ssh ${ssh_user}@${deploy_host} "sudo -u ${site_owner} php ${site_path}/artisan migrate"
