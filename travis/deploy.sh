b712263562c52d1ea0a494395fcf81d5fcd95d4e
Evan Hsu <evanhsu@gmail.com>
2017-07-31 01:44:17 -0600

    Update deploy script.

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -3,7 +3,7 @@
 # Deployment Script
 
 ssh $ssh_user@$deploy_host 'mkdir -p ~/deploy'
-
-rsync -a --quiet --delete-after --exclude .git $TRAVIS_BUILD_DIR $ssh_user@$deploy_host:~/deploy
+echo $TRAVIS_BUILD_DIR
+rsync -a --quiet --delete-after --exclude .git* $TRAVIS_BUILD_DIR $ssh_user@$deploy_host:~/deploy
 
 ssh $ssh_user@$deploy_host 'sudo cp -a ~/deploy/. $deploy_path/ && sudo chown -R $site_owner:$site_owner $deploy_path/'
