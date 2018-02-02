ea722b4009ea4aafdc0512c4757855eaa47b887a
Evan Hsu <evanhsu@gmail.com>
2018-02-02 02:01:12 -0700

    Menubar highlights the correct button on the Summary page.

--- app/Http/Controllers/Status/SummaryController.php
+++ app/Http/Controllers/Status/SummaryController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers\Status;
 
 use App\Http\Controllers\Controller;
 use App\Domain\Crews\Crew;
+use Illuminate\Http\Request;
 
 class SummaryController extends Controller
 {
     /**
      *
+     * @param Request $request
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $crews = Crew::with(['status', 'statusableResources.latestStatus'])->get();
+
+        $request->session()->flash('active_menubutton', 'summary'); // Tell the menubar which button to highlight
+
         return view('summary')->with('crews', $crews);
     }
 }
