7cd07b5f93305d79d86576d5f7bcde87316415d8
Evan Hsu <evanhsu@gmail.com>
2018-02-12 16:32:10 -0700

    Removed the 'Crew Type' dropdown from the new Crew form.

--- resources/views/crews/new.blade.php
+++ resources/views/crews/new.blade.php
@@ -30,18 +30,6 @@
             </div>
 
             <div class="form-group">
-                <label for="statusable_type" class="col-sm-3 control-label">Crew Type</label>
-                <div class="col-sm-6">
-                    <select name="statusable_type" id="statusable_type" class="form-control">
-                        <option value="Crew">Hotshots</option>
-                        <option value="Rappelhelicopter">Rappel</option>
-                        <option value="Shorthaulhelicopter">Short Haul</option>
-                        <option value="Smokejumperairplane">Smokejumpers</option>
-                    </select>
-                </div>
-            </div>
-
-            <div class="form-group">
                 <div class="col-sm-offset-3 col-sm-2">
                     <button type="submit" class="btn btn-default">Create</button>
                 </div>
