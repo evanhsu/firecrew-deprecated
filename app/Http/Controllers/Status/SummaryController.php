edd9e804378f60e2a3c091d6754b6f484cbb3934
Evan Hsu <evanhsu@gmail.com>
2018-02-13 23:45:25 -0700

    Summary page most recent timestamp between CrewStatus and all ResourceStatuses for a Crew.

--- app/Http/Controllers/Status/SummaryController.php
+++ app/Http/Controllers/Status/SummaryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Status;
 use App\Http\Controllers\Controller;
 use App\Domain\Crews\Crew;
 use Illuminate\Http\Request;
+
 //use App\Http\Transformers\CrewTransformer;
 //use League\Fractal\Manager;
 
@@ -26,8 +27,21 @@ class SummaryController extends Controller
     public function indexApi(Request $request)
     {
         $crews = $this->getData();
+        $modifiedCrews = $crews->map(function ($crew) {
+            $mostRecentTimestamp = $crew->status->updated_at;
 
-        return $crews->toJson();
+            if (!is_null($crew->statusableResources)) {
+                $crew->statusableResources->each(function ($resource) use (&$mostRecentTimestamp) {
+                    if ($resource->latestStatus->updated_at->gt($mostRecentTimestamp)) {
+                        $mostRecentTimestamp = $resource->latestStatus->updated_at;
+                    }
+                });
+                $crew->status->updated_at = $mostRecentTimestamp;
+            }
+            return $crew;
+        });
+
+        return $modifiedCrews->toJson();
 
 //        return $this->response->collection(
 //            $crews,
