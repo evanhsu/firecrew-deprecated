d4ba626b9eea4b2b8d0dcb06566b62dabf11c0bc
Evan Hsu <evanhsu@gmail.com>
2018-02-21 14:36:58 -0700

    Corrected syntax of rsync exclude-from= in the travis/deploy.sh script

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -4,7 +4,7 @@
 
 ssh ${ssh_user}@${deploy_host} "mkdir -p ~/deploy"
 
-rsync -a --quiet --delete-after --exclude_from travis/exclude_from_deploy ${TRAVIS_BUILD_DIR}/ ${ssh_user}@${deploy_host}:~/deploy
+rsync -a --quiet --delete-after --exclude-from=travis/exclude_from_deploy ${TRAVIS_BUILD_DIR}/ ${ssh_user}@${deploy_host}:~/deploy
 
 ssh ${ssh_user}@${deploy_host} "sudo cp -a ~/deploy/. ${deploy_path}/"
 ssh ${ssh_user}@${deploy_host} "sudo chown -RP --quiet --preserve-root ${site_owner}:${site_owner} ${deploy_path}"
