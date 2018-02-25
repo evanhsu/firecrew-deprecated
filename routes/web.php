e8413f63eebcc87f11f379828ab053b3527003e7
Evan Hsu <evanhsu@gmail.com>
2018-02-25 15:03:48 -0700

    Added news alert about the /summary url being updated soon

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,8 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', array('as' => 'summary', 'uses' => 'Status\SummaryController@index'));
+Route::get('/', array('uses' => 'Status\SummaryController@indexTempRedirect'));
+Route::get('/summary', array('as' => 'summary', 'uses' => 'Status\SummaryController@index'));
 Route::get('/map', array('as' => 'map', 'uses' => 'MapController@getMap'));
 Route::get('/home', 'HomeController@index');
 Route::get('/privacy', 'PagesController@privacy');
