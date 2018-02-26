8cb4522e3181e602aa4d39fca3ef15a2c35fff5d
Evan Hsu <evanhsu@gmail.com>
2018-02-25 22:57:19 -0700

    Fixed crew->updated_at timestamp not being updated when a new resource_status or crew_status is created.

--- app/Domain/Statuses/CrewStatus.php
+++ app/Domain/Statuses/CrewStatus.php
@@ -9,21 +9,13 @@ class CrewStatus extends Model
 {
 
     protected $table = "crew_statuses";
-
+    protected $touches = ["crew"];
     /**
      * The attributes that are NOT mass assignable.
      *
      * @var array
      */
     protected $guarded = [];
-
-    protected static function boot()
-    {
-        parent::boot();
-        self::created(function ($model) {
-            $model->crew->update(['updated_at' => $model->created_at]);
-        });
-    }
 
     public function crew()
     {
