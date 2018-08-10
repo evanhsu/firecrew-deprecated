9d236e135ca1f53b829407868b5b18a7cf4e20ba
Evan Hsu <evanhsu@gmail.com>
2018-08-10 09:55:55 -0600

    Timestamp component now updates its relative time every 10 seconds, so strings like "30 seconds ago" will automatically change to "1 minute ago" without a page refresh.

--- resources/assets/js/components/StatusSummaryTable/Timestamp.js
+++ resources/assets/js/components/StatusSummaryTable/Timestamp.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Moment from 'moment';
 import momentTz from 'moment-timezone';
@@ -10,10 +10,39 @@ const localDateString = (utcDateString) => {
   return `${Moment.utc(utcDateString).tz(localTimeZone).calendar()} ${localTimeZoneAbbr}`;
 };
 
-const Timestamp = ({ timestamp }) => (
-  timestamp === undefined ? null :
-    (<span style={{ fontSize: 11 }}>Update: {localDateString(timestamp)} ({Moment.utc(timestamp).fromNow()})</span>)
-);
+class Timestamp extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      timeString: props.timestamp === undefined ? null : localDateString(props.timestamp),
+      relativeTime: props.timestamp === undefined ? '' : Moment.utc(props.timestamp).fromNow(),
+    }
+  }
+
+  componentDidMount() {
+    this.timeout();
+  }
+
+  timeout() {
+    setTimeout(() => this.updateRelativeTime(), 10000);
+  }
+
+  updateRelativeTime = () => {
+    this.setState({
+      relativeTime: props.timestamp === undefined ? '' : Moment.utc(props.timestamp).fromNow(),
+    })
+  };
+
+  render() {
+    if(props.timestamp === null) {
+      return null;
+    }
+
+    return (<span style={{ fontSize: 11 }}>Update: {this.state.timeString} ({this.state.relativeTime})</span>);
+
+  }
+}
 
 Timestamp.propTypes = {
   timestamp: PropTypes.node,
